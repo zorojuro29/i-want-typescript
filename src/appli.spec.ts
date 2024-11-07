cd387975f0cb482eb1dc26556464b9829a9e88a3
zorojuro29 <julien.struillou@ensta-bretagne.org>
2024-11-07 11:40:16 +0100

    suppression fichiers app.spec.ts hello-world.spec.ts

--- src/appli.spec.ts
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-import { AddressInfo } from 'net';
-import si from 'systeminformation';
-import './index';
-
-describe('Tests unitaires pour notre application', () =>{
-    
-})
--- src/hello-world.spec.ts
+++
@@ -1,8 +0,0 @@
-import { helloWorld } from './hello-world';
-
-describe('typeScript test suite', () => {
-  it('should return "Hello world!"', () => {
-    expect.assertions(1);
-    expect(helloWorld()).toBe('Hello, World!');
-  });
-});
